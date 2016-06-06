443a6d4cd34899160115863ba0caeff65eb6be57
Vlad <BoBaHbl4@gmail.com>
2016-06-07 02:25:26 +0300

    Language routing update.

--- dev_root/js/controllers/main.controller.js
+++ dev_root/js/controllers/main.controller.js
@@ -18,33 +18,52 @@
 
         // Language definition
 
-        var setLangCurrent = function () {
+        // var setLangCurrent = function () {
+        //
+        //     $scope.langIdCurrent = null;
+        //     $scope.langNameCurrent = null;
+        //     $scope.locationPath = [];
+        //
+        //     $scope.locationPath = $location.path().split("/");
+        //     $scope.langIdCurrent = $scope.locationPath[1];
+        //     angular.forEach($rootScope.langArray, function(value){
+        //         if ($scope.langIdCurrent == value.langId) {
+        //             console.log(value.langId);
+        //             $scope.langNameCurrent = value.langName;
+        //             return $scope.langNameCurrent;
+        //         }
+        //     });
+        // };
+        //
+        //setLangCurrent();
 
-            $scope.langIdCurrent = null;
-            $scope.langNameCurrent = null;
-            $scope.locationPath = [];
+        $scope.langIdCurrent = null;
+        $scope.langNameCurrent = null;
+        $scope.locationPath = [];
 
-            $scope.locationPath = $location.path().split("/");
-            $scope.langIdCurrent = $scope.locationPath[1];
-            angular.forEach($rootScope.langArray, function(value){
-                if ($scope.langIdCurrent == value.langId) {
-                    console.log(value.langId);
-                    $scope.langNameCurrent = value.langName;
-                    return $scope.langNameCurrent;
-                }
-            });
-        };
-
-        setLangCurrent();
-
-        console.log($scope.langNameCurrent);
+        $scope.locationPath = $location.path().split("/");
+        $scope.langIdCurrent = $scope.locationPath[1];
+        angular.forEach($rootScope.langArray, function(value){
+            if ($scope.langIdCurrent == value.langId) {
+                console.log('loaded "' + value.langId + '"');
+                $scope.langNameCurrent = value.langName;
+                return $scope.langNameCurrent;
+            }
+        });
 
         // Set Url For Any "Data Get Service"
         var apiUrl = appConstant.API_URL;
 
         $scope.changeLanguage = function(language) {
             $state.go($state.current.name, {language: language});
-            setLangCurrent();
+
+            angular.forEach($rootScope.langArray, function(value){
+                if (language == value.langId) {
+                    console.log('switched to "' + value.langId +'"');
+                    $scope.langNameCurrent = value.langName;
+                    return $scope.langNameCurrent;
+                }
+            });
         }
 
     }
